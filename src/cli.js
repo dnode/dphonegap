d34a7226f1b4b023b0a3d65704e4f2df0584f027
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-03-08 16:31:41 +0100

    Add "www/index.html" as mandatory file and inject the cordova specific javascript files

--- src/cli.js
+++ src/cli.js
@@ -2,9 +2,14 @@
 'use strict';
 
 const copyfiles = require('copyfiles');
+const fs = require('fs');
 
 let dependency = process.argv[2];
 let paths = require(process.cwd() + '/node_modules/' + dependency + '/phonegap.json');
+paths.push('www/index.html');
 paths = paths.map((path) => { return 'node_modules/' + dependency + '/' + path });
 paths.push('../');
-copyfiles(paths, 2, () => {});
+copyfiles(paths, 2, () => {
+  const path = '../www/index.html';
+  fs.writeFileSync(path, fs.readFileSync(path, 'utf8').replace('</body>', "<script src=\"cordova.js\"></script>\n<script src=\"app-cordova.js\"></script>\n</body>"));
+});
